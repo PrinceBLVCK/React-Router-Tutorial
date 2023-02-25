84634a8459d8d9d2d3d40d2a5c6582d6994ffd3d
PrinceBLVCK <princekarabo70@gmail.com>
2023-02-25 14:56:27 +0200

    adding a filter by using the searchParams function.

--- src/components/users.js
+++ src/components/users.js
@@ -1,7 +1,11 @@
 import { Outlet } from "react-router"
-import { Link } from "react-router-dom"
+import { Link, useSearchParams } from "react-router-dom"
 
 const Users = () =>{
+
+    const [searchParams, setSearchParams] = useSearchParams()
+    const showActiveUsers = searchParams.get('filter') === 'active'
+
     return(
         <>
             <h2> List of Users</h2>
@@ -9,8 +13,18 @@ const Users = () =>{
             <h3><Link to='2'>User 2</Link></h3>
             <h3><Link to='3'>User 3</Link></h3>
             <h3><Link to='admin'>Admin</Link></h3>
-            
             <Outlet />
+
+            <div>
+                <button onClick={() => setSearchParams({filter: 'active'})}>Active Users</button> 
+                <button onClick={() => setSearchParams({})}>Reset Filter</button>
+
+                {showActiveUsers ? ( <h5>showing Active Users</h5> ) : ( <h5>showing All Users</h5> )}
+
+
+            </div>
+
+
         </>
     )
 }
